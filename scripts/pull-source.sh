e7eb5bda4c1ffee05f86423769dc4182023330df
Eric Liao <rcliao01@gmail.com>
2017-08-19 22:55:10 -0700

    refactor: better script pwd

--- scripts/pull-source.sh
+++ scripts/pull-source.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+SUBJECT=$1
+# Make sure the directory is at the repository root
+cd $( dirname $0 ) && cd ..
+
+# Check if the remote is already set
 git ls-remote --exit-code course&>/dev/null
 if test $? != 0; then
     echo "Course remote doesn't exist. Setting course remote to 'git@github.com:csula/cs4660-fall-2017.git'"
@@ -9,7 +14,8 @@ fi
 # Need to fetch the remote first prior to download
 git fetch course
 
-echo "Downloading $1 from course repository ..."
-git checkout course/master -- cs4660/$1
+echo "Downloading $SUBJECT from course repository ..."
+git checkout course/master -- cs4660/$SUBJECT
 echo "Redownloading test codes"
 git checkout course/master -- cs4660/test
+
