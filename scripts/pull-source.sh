d01d673256265ca08ab4c7d99fda56648cd0d276
Eric Liao <rcliao01@gmail.com>
2017-08-20 13:10:23 -0700

    fix: pull source script to use https
    Due to the reason many students may not set up their ssh key properly,
    use https will be a safer pick

--- scripts/pull-source.sh
+++ scripts/pull-source.sh
@@ -7,8 +7,8 @@ cd $( dirname $0 ) && cd ..
 # Check if the remote is already set
 git ls-remote --exit-code course&>/dev/null
 if test $? != 0; then
-    echo "Course remote doesn't exist. Setting course remote to 'git@github.com:csula/cs4660-fall-2017.git'"
-    git remote add course git@github.com:csula/cs4660-fall-2017.git
+    echo "Course remote doesn't exist. Setting course remote to 'https://github.com/csula/cs4660-fall-2017.git'"
+    git remote add course https://github.com/csula/cs4660-fall-2017.git
 fi
 
 # Need to fetch the remote first prior to download
