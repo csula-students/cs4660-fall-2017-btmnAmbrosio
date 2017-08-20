fa16f0392404312186c4a79c6cdec62b5e9e7d2b
Eric Liao <rcliao01@gmail.com>
2017-08-19 19:58:11 -0700

    chore: update pull-source script

--- scripts/pull-source.sh
+++ scripts/pull-source.sh
@@ -6,5 +6,10 @@ if test $? != 0; then
     git remote add course git@github.com:csula/cs4660-fall-2017.git
 fi
 
+# Need to fetch the remote first prior to download
+git fetch course
+
 echo "Downloading $1 from course repository ..."
 git checkout course/master -- cs4660/$1
+echo "Redownloading test codes"
+git checkout course/master -- cs4660/test
