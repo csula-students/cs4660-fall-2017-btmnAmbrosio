5336ad5fdfe6232ff80eca1d41c78c0c393d6e13
Eric Liao <rcliao01@gmail.com>
2017-09-01 22:24:06 -0700

    feat: add simple file read tests

---
+++ cs4660/tutorial/files.py
@@ -0,0 +1,38 @@
+"""Files tests simple file read related operations"""
+
+class SimpleFile(object):
+    """SimpleFile tests using file read api to do some simple math"""
+    def __init__(self, file_path):
+        self.numbers = []
+        """
+        TODO: reads the file by path and parse content into two
+        dimension array (numbers)
+        """
+
+    def get_mean(self, line_number):
+        """
+        get_mean retrieves the mean value of the list by line_number (starts
+        with zero)
+        """
+        pass
+
+    def get_max(self, line_number):
+        """
+        get_max retrieves the maximum value of the list by line_number (starts
+        with zero)
+        """
+        pass
+
+    def get_min(self, line_number):
+        """
+        get_min retrieves the minimum value of the list by line_number (starts
+        with zero)
+        """
+        pass
+
+    def get_sum(self, line_number):
+        """
+        get_sum retrieves the sumation of the list by line_number (starts with
+        zero)
+        """
+        pass
