0e8c3e93f1e158ef5d9f2d5840144de209c6dc7b
Ambrosio <jjambrosio1@gmail.com>
2017-09-09 08:16:45 -0700

    completed python exercise

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -4,6 +4,26 @@ class SimpleFile(object):
     """SimpleFile tests using file read api to do some simple math"""
     def __init__(self, file_path):
         self.numbers = []
+        from io import open 
+        f = open(file_path, encoding='utf-8')
+        for rows in f:
+            self.numbers.append(rows.split())
+        
+        # for rows in self.numbers:
+        #     for nums in rows:
+        #         nums= int(nums)
+
+        # from builtins import map
+        # self.numbers = list(map(int,self.numbers))
+
+        self.numbers=[[int(y) for y in x] for x in self.numbers]
+
+        
+        
+        f.close()
+
+
+
         """
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
@@ -14,25 +34,29 @@ class SimpleFile(object):
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        pass
+        return (sum(self.numbers[line_number])/(len(self.numbers[line_number])))
+
+        
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        pass
+        return max(self.numbers[line_number])
+
+
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        pass
+        return min(self.numbers[line_number])
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        pass
+        return sum(self.numbers[line_number])
--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -2,20 +2,27 @@
 
 def get_first_item(li):
     """Return the first item from the list"""
-    pass
+    return li[0]
 
 def get_last_item(li):
     """Return the last item from the list"""
-    pass
+    return li[len(li)-1]
 
 def get_second_and_third_items(li):
     """Return second and third item from the list"""
-    pass
+    return li[1:3]
 
 def get_sum(li):
     """Return the sum of the list items"""
-    pass
+    x = 0
+    for i in li:
+        x= x + i
+    return x
 
 def get_avg(li):
     """Returns the average of the list items"""
-    pass
+    x = 0.0
+    for i in li:
+        x= x + i
+    x = x / len(li) 
+    return x
